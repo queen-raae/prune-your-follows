c82e9084a720b821e4a9452576277cac5acc165e
Benedicte Raae <raae@raae.codes>
2022-08-23 16:54:13 +0200

    refactor: back to supabase v1

--- src/domains/app/useUser.js
+++ src/domains/app/useUser.js
@@ -2,21 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import { supabase } from "./supabaseClient";
 
 export default function useUser() {
-  return useQuery(
-    ["user"],
-    async () => {
-      const result = await supabase.auth.getSession();
+  return useQuery(["user"], async () => {
+    const session = supabase.auth.session();
 
-      // console.log(result.data.session?.user);
+    // console.log("useUser", session.user);
 
-      if (result.error) {
-        throw result.error;
-      } else {
-        return result;
-      }
-    },
-    {
-      select: (result) => result.data.session?.user,
-    }
-  );
+    return session?.user;
+  });
 }
