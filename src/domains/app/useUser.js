3cdb13887d33e1af17bc3b3f61a5d62b10ab8ccb
Benedicte Raae <raae@raae.codes>
2022-10-27 14:32:52 +0200

    feat: redirect to root when unauthenticated

--- src/domains/app/useUser.js
+++ src/domains/app/useUser.js
@@ -1,8 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
+import { navigate } from "gatsby";
 import { useSession } from "next-auth/react";
 
 export default function useUser() {
-  const { data: session } = useSession();
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated: () => {
+      navigate("/");
+    },
+  });
   const user = session?.user;
 
   return useQuery(["user", user?.id], async () => {
