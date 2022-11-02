8313451d1b8f224516cbfff61bb0781088fb38c0
Benedicte Raae <raae@raae.codes>
2022-11-02 14:55:35 +0100

    fix: ssg error

--- src/domains/app/filter/FilterHeader.js
+++ src/domains/app/filter/FilterHeader.js
@@ -5,9 +5,9 @@ export function FilterHeader({ filter, as = "div", ...props }) {
   const Component = as;
   return (
     <Component {...props} className="group flex items-center">
-      <h2 className="text-md inline font-bold">{filter.name}</h2>
+      <h2 className="text-md inline font-bold">{filter?.name}</h2>
       <p className="inline-block pl-2 align-baseline opacity-80">
-        {filter.description}
+        {filter?.description}
         {props.to && (
           <ArrowLongRightIcon className="inline-block h-4 pl-2 opacity-60 transition group-hover:translate-x-1 group-hover:opacity-100" />
         )}
