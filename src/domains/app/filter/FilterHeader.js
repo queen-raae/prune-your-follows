128440ffed00018d823e96d47081794fcaf78665
Benedicte Raae <raae@raae.codes>
2022-11-10 20:27:05 +0100

    fix: a little mobile

--- src/domains/app/filter/FilterHeader.js
+++ src/domains/app/filter/FilterHeader.js
@@ -8,17 +8,15 @@ export function FilterHeader({ filter, as = "div", className, ...props }) {
     <Component
       {...props}
       className={clsx(
-        "group block items-center leading-snug sm:flex",
+        "group flex flex-wrap items-center leading-snug",
         className
       )}
     >
       <h2 className="text-md mr-2 font-bold">{filter?.name}</h2>
-      <p className="align-baseline opacity-80">
-        {filter?.description}
-        {props.to && (
-          <ArrowLongRightIcon className="inline-block h-4 pl-2 opacity-60 transition group-hover:translate-x-1 group-hover:opacity-100" />
-        )}
-      </p>
+      {props.to && (
+        <ArrowLongRightIcon className="block h-4 pl-2 opacity-60 transition group-hover:translate-x-1 group-hover:opacity-100 lg:order-last" />
+      )}
+      <p className="opacity-80">{filter?.description}</p>
     </Component>
   );
 }
