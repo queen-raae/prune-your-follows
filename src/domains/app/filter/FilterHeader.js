5b9da1ebe448bfe4320ee835ab34d945b41ade58
Benedicte Raae <raae@raae.codes>
2022-11-10 20:13:16 +0100

    fix: xs styling

--- src/domains/app/filter/FilterHeader.js
+++ src/domains/app/filter/FilterHeader.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { ArrowLongRightIcon } from "@heroicons/react/20/solid";
+import clsx from "clsx";
 
-export function FilterHeader({ filter, as = "div", ...props }) {
+export function FilterHeader({ filter, as = "div", className, ...props }) {
   const Component = as;
   return (
-    <Component {...props} className="group flex items-center">
-      <h2 className="text-md inline font-bold">{filter?.name}</h2>
-      <p className="inline-block pl-2 align-baseline opacity-80">
+    <Component
+      {...props}
+      className={clsx(
+        "group block items-center leading-snug sm:flex",
+        className
+      )}
+    >
+      <h2 className="text-md mr-2 font-bold">{filter?.name}</h2>
+      <p className="align-baseline opacity-80">
         {filter?.description}
         {props.to && (
           <ArrowLongRightIcon className="inline-block h-4 pl-2 opacity-60 transition group-hover:translate-x-1 group-hover:opacity-100" />
