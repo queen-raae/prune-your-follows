e82a3f9a5b831fc3c8330e2f387d0bc6b89f8448
Benedicte Raae <raae@raae.codes>
2022-11-24 11:09:46 +0100

    wip

--- src/api/accounts.js
+++ src/api/accounts.js
@@ -47,6 +47,7 @@ export default async function handler(req, res) {
       if (validationError) {
         throw createError.UnprocessableEntity(validationError);
       }
+      CHANGE TO USER ID TO BE CONSISTENT
       res.send(await getAccounts({ ...value, followerId: token.sub }));
     } else {
       throw createError(405, `${req.method} not allowed`);
