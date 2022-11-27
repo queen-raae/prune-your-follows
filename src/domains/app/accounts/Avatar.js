c7063d4ee0463b60b328ea2a8dfdd2fefb2fe40d
Benedicte Raae <raae@raae.codes>
2022-11-27 20:26:38 +0100

    fix: broken image avatars (#37)
    * refactor: avatar in its own component
    
    * feat: hide on broken image, pulse on loading
    
    * fix: flip logic, hidden until visible
    
    * feat: use initials when image is broken

---
+++ src/domains/app/accounts/Avatar.js
@@ -0,0 +1,50 @@
+import clsx from "clsx";
+import React, { useState } from "react";
+
+export function Avatar({ imageUrl, altText, className, name = "" }) {
+  const [status, setStatus] = useState("loading");
+  let initials = name
+    .split(" ")
+    .map((word) => Array.from(word).slice(0, 1))
+    .slice(0, 2)
+    .join("");
+
+  const handleOnLoad = () => {
+    setStatus("fulfilled");
+  };
+
+  const handleOnError = () => {
+    setStatus("failed");
+  };
+
+  return (
+    <div
+      className={clsx(
+        "overflow-hidden rounded-full border bg-slate-400",
+        "flex items-center justify-center",
+        status === "loading" ? "animate-pulse" : "",
+        className
+      )}
+    >
+      <img
+        className={clsx(
+          status === "fulfilled" ? "visible" : "hidden",
+          "w-full"
+        )}
+        src={imageUrl}
+        alt={altText}
+        onLoad={handleOnLoad}
+        onError={handleOnError}
+      />
+      <span
+        aria-hidden={true}
+        className={clsx(
+          status === "failed" ? "visible" : "hidden",
+          "text-slate-100"
+        )}
+      >
+        {initials}
+      </span>
+    </div>
+  );
+}
