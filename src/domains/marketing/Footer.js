46b6fc8655afe7e7c0fe35fcc8faa6731d6f8a7d
Benedicte Raae <raae@raae.codes>
2022-12-05 10:24:17 +0100

    fix spelling error

--- src/domains/marketing/Footer.js
+++ src/domains/marketing/Footer.js
@@ -15,7 +15,7 @@ export function Footer() {
           >
             Queen Raae
           </a>
-          , power by{" "}
+          , powered by{" "}
           <a
             className="underline"
             href="https://xata.io/?utm_campaign=prune+your+follows&utm_source=app&utm_medium=footer"
@@ -50,7 +50,7 @@ export function Footer() {
           >
             <svg
               aria-hidden="true"
-              className="h-6 w-6 fill-green-700 group-hover:fill-green-900"
+              className="otransition h-6 w-6 fill-green-700 group-hover:translate-x-1 group-hover:fill-green-900"
             >
               <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0 0 22 5.92a8.19 8.19 0 0 1-2.357.646 4.118 4.118 0 0 0 1.804-2.27 8.224 8.224 0 0 1-2.605.996 4.107 4.107 0 0 0-6.993 3.743 11.65 11.65 0 0 1-8.457-4.287 4.106 4.106 0 0 0 1.27 5.477A4.073 4.073 0 0 1 2.8 9.713v.052a4.105 4.105 0 0 0 3.292 4.022 4.093 4.093 0 0 1-1.853.07 4.108 4.108 0 0 0 3.834 2.85A8.233 8.233 0 0 1 2 18.407a11.615 11.615 0 0 0 6.29 1.84" />
             </svg>
