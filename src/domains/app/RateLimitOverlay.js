890b1b4ad0a73a3db84c3cb8d12063d4f727b43a
Benedicte Raae <raae@raae.codes>
2022-12-15 21:35:30 +0100

    fix: positive rate limit note

--- src/domains/app/RateLimitOverlay.js
+++ src/domains/app/RateLimitOverlay.js
@@ -10,29 +10,26 @@ export function RateLimitOverlay({ open }) {
   return (
     <Modal open={open} Icon={Icon} title="Twitter needs a break">
       <p>
-        Unfortunately, there are some strict limits on the number of unfollows
-        Prune your Follows may facilitate daily{" "}
-        <strong>across all users</strong>. The success of today’s Product Hunt
-        Launch got us over pretty fast. On any other day, you should be able to
-        unfollow 50 accounts every 15 minutes.
+        Wow, thank you for the love! Today's success has pushed us over the
+        limit of the number of unfollows PYF's is allowed to facilitate{" "}
+        <strong>across</strong> all users. Come back tomorrow, and you should be
+        able to unfollow 50 accounts per 15 minutes!
       </p>
 
       {user.email ? (
         <>
           <p className="my-3">
-            Come back again in a couple of days! No worries, you'll get a
-            reminder in your inbox.
+            No worries, you'll get a reminder in your inbox.
           </p>
           <LogoutButton className="flex w-full items-center justify-center rounded-md border border-transparent bg-green-900 px-3 py-2 text-sm font-medium text-orange-50 shadow-sm sm:px-8" />
         </>
       ) : (
         <>
           <h4 className="mt-4 text-base font-medium leading-loose">
-            Get notified!
+            Need a reminder?
           </h4>
           <p className="mb-3">
-            Sign up to be notified when Twitter is all rested and ready to roll
-            again.
+            Sign up for a reminder to come back when PYF is not such hot news.
           </p>
           <EmailForm />
         </>
