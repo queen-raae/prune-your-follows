798b2429af8b881ccdd60d7e0bb8f68aada0adea
Benedicte Raae <raae@raae.codes>
2022-12-15 08:52:33 +0100

    feat: product hunt badge

--- src/domains/marketing/Hero.js
+++ src/domains/marketing/Hero.js
@@ -28,7 +28,18 @@ export function Hero() {
               Find Twitter accounts you follow that no longer spark joy,
               unfollow them and make room for new connections to grow 🌱
             </p>
-            <div className="mx-auto mt-10 flex justify-center">
+            <div className="mx-auto mt-10 flex justify-center gap-4">
+              <a
+                href="https://www.producthunt.com/posts/prune-your-follows-a-twitter-tool?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-prune&#0045;your&#0045;follows&#0045;a&#0045;twitter&#0045;tool"
+                target="_blank"
+              >
+                <img
+                  src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=371207&theme=dark"
+                  alt="Prune&#0032;your&#0032;Follows&#0032;&#0045;&#0032;a&#0032;Twitter&#0032;tool - Does&#0032;your&#0032;Twitter&#0032;timeline&#0032;bring&#0032;you&#0032;joy&#0063; | Product Hunt"
+                  width="250"
+                  height="54"
+                />
+              </a>
               <LoginButton className="hover:bg-brand-50 flex items-center justify-center rounded-md border border-transparent bg-orange-50 px-4 py-3 text-base font-medium text-emerald-900 shadow-sm sm:px-8">
                 Sign in with Twitter
               </LoginButton>
