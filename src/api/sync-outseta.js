02cd2c347be210cd4431b6eaab92287132fc5df4
raae <raae@raae.codes>
2023-04-14 10:40:49 +0200

    feat: sync outseta

--- src/api/sync-outseta.js
+++ src/api/sync-outseta.js
@@ -1,32 +1,71 @@
 import "../domains/api/fetch-polyfill";
-
+import axios from "axios";
 import { getXataClient } from "../domains/xata";
+
 const xata = getXataClient();
 
+const outsetaApi = axios.create({
+  baseURL: "https://raae-testing.outseta.com/api/v1/",
+  headers: {
+    Authorization: `Outseta ${process.env.OUTSETA_KEY}:${process.env.OUTSETA_SECRET}`,
+  },
+});
+
 export default async function handler(req, res) {
-  const page = await xata.db.meta.getPaginated({
+  let page = await xata.db.meta.getPaginated({
     pagination: {
-      size: 15,
+      size: 3,
     },
   });
 
+  let successCounter = 0;
+
   do {
     try {
-      // Success update count
-      count += records.length;
+      // For each record in Xata
+      // update the person in Outseta
+      // or create a person in Outseta and save the outseta id to Xata
+
+      for (const record of page.records) {
+        if (record.outsetaId) {
+          const { data: outsetaData } = await outsetaApi.put(
+            `crm/people/${record.outsetaId}`,
+            {
+              Email: record.email,
+            }
+          );
+
+          console.log("Updated outseta person", outsetaData);
+        } else {
+          const { data: outsetaData } = await outsetaApi.post(`crm/people`, {
+            Email: record.email,
+            TwitterId: record.id,
+          });
+
+          console.log("Added outseta person", outsetaData);
+
+          const recordResult = await xata.db.meta.update({
+            id: record.id,
+            outsetaId: outsetaData.Uid,
+          });
+
+          console.log("Updated xata record", recordResult);
+        }
+
+        // Success update count
+        successCounter += 1;
+      }
 
       if (page.hasNextPage()) {
         page = await page.nextPage();
       } else {
         page = null;
-        res.send(`Updated all ${count} accounts`);
+        res.send(`Synced all ${successCounter} users`);
       }
     } catch (error) {
-      console.log(error.message);
       page = null;
-      res.send(`Updated: ${count} accounts`);
+      console.log(error);
+      res.send(`Error while syncing, finished ${successCounter} users`);
     }
   } while (page);
-
-  res.send("ok");
 }
